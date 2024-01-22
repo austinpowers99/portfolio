a6a815485a447f4492ec9891d1c1772bcb741a88
austinpowers99 <samanthaf_9@outlook.com>
2024-01-22 10:28:28 -0800

    add typewriter-text in the html file to showcase hobbies & interests

--- app.js
+++ app.js
@@ -6,21 +6,3 @@ function toggleEmail() {
         emailElement.style.display = "none";
     }
 }
-
-        // // Function to set and animate the typewriter text
-        // function typeWriter(text, speed) {
-        //     let i = 0;
-        //     const typeInterval = setInterval(function() {
-        //         if (i < text.length) {
-        //             typewriterText.textContent += text.charAt(i);
-        //             i++;
-        //         } else {
-        //             clearInterval(typeInterval); // Stop typing when text is complete
-        //         }
-        //     }, speed);
-        // }
-
-        // // Call the typewriter function with the desired text and speed
-        // const textToType = ''; // Change this to your desired text
-        // const typingSpeed = 100; // Adjust typing speed here (milliseconds)
-        // typeWriter(textToType, typingSpeed);
